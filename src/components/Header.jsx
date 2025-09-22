c651adf462b9543a9b40459ce3ea7e17a5817a63
D.P. Karavita <ar112689@fhss.sjp.ac.lk>
2025-09-23 01:19:52 +0530

    style(Header): Add horizontal alignment, background color and other styles with Tailwind CSS

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom';
 
 
 const Header=()=>{
-    return (<header> <nav>
-        <ul>
+    return (<header className="flex justify-between items-center p-4 bg-black text-white" > <nav>
+        <ul className="flex space-x-4 ">
           <li><Link to="/">Home</Link></li>
           <li><Link to="/about">About</Link></li>
           <li><Link to="/projects">Project</Link></li>
