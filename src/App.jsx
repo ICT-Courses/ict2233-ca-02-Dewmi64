99f73d196e3d5dff7f039c1c517ce53ab8f9ae0a
D.P. Karavita <ar112689@fhss.sjp.ac.lk>
2025-09-23 00:57:23 +0530

    Configure routing for all pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
 import { useState } from 'react'
 import './App.css'
 import './styles/main.scss';
-import Header from './componets/Header';
+import Header from './components/Header';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
+import ProjectPage from './pages/ProjectPage';
+import { Routes, Route } from 'react-router-dom';
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-   <div> <Header />
-    <div>My Portfolio</div>
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/projects" element={<ProjectPage />} />
+      </Routes>
     </div>
-      
-  )
+  );
 }
 
-export default App
+export default App;
