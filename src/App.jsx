7135e216fd6026f68ac89925d3cb88d4516d8a2b
D.P. Karavita <ar112689@fhss.sjp.ac.lk>
2025-09-23 00:08:58 +0530

    Add Header component with navigation links

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import './styles/main.scss';
-
+import Header from './componets/Header';
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+   <div> <Header />
     <div>My Portfolio</div>
+    </div>
       
   )
 }
