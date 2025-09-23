509af7330417db20e9f131b09a49b82231e42f6e
D.P. Karavita <ar112689@fhss.sjp.ac.lk>
2025-09-23 08:25:15 +0530

    Add hero section to home page with initial styling

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 const HomePage = () => {
   return (
+    <section className="flex justify-around items-center p-10 space-x-10 lg:flex-row ssm:w-fit bg-blue-900">
     <div>
       <h1>Home Page</h1>
     </div>
+    </section>
   );
 };
 
