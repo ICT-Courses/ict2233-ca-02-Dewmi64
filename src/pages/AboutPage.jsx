2e7ef9d9cf158c60dd9c4c883030e5478d5bb543
D.P. Karavita <ar112689@fhss.sjp.ac.lk>
2025-09-23 19:56:42 +0530

    Add "About Me" section to AboutPage

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -2,9 +2,17 @@ import React from 'react';
 
 const AboutPage = () => {
   return (
+    <section className= "flex-1 space-y-8">
     <div>
-      <h1>About Page</h1>
+      <h1 className="text-4xl md:text-5xl font-bold text-[#f8fafc] mb-4">
+       <span className="text-[#cbd5e1]">About Me</span></h1>
+    <p className="text-lg md:text-xl leading-relaxed">
+              I am an undergraduate IT student at the University of Sri Jayewardenepura, pursuing a BSc in ICT. 
+              I combine technical knowledge with creativity to build effective digital solutions. My interests include UI/UX design and project management. 
+              I aim to grow into a role as a Project Manager, UI/UX Designer, or Business Analyst, contributing to impactful projects while continuously learning in the IT industry.
+            </p>
     </div>
+    </section>
   );
 };
 
