85f701794a3f41aafd08f65360293ed05924433e
D.P. Karavita <ar112689@fhss.sjp.ac.lk>
2025-09-24 11:50:21 +0530

    Update home page layout and change profile photo

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,16 +4,19 @@ import ProfilePicture from '../assets/Image/Profile Pic.jpg';
 
 const HomePage = () => {
   return (
-  <section className="flex flex-col md-flex-row justify-center items-center p-10 space-x-10 lg:flex-row ssm:w-fit bg-[#0f172a] min-h-screen">
+  <section className="flex flex-col-reverse md-flex-row justify-center items-center p-10 space-x-10 lg:flex-row ssm:w-fit bg-[#0f172a] min-h-screen">
   <div className='flex flex-col justify-center itemscenter md:items-start text-center md:text-left md:lg:w 1/3 '>
     <p className ='text-4xl mb-5  md:text-5xl text-slate-300'>Hii, I'm</p>
-      <h1 className="text-6xl  md:text-7xl text-white mb-5">Dewmini Karavita</h1>
+      <h1 className="text-4xl md:text-5xl font-bold text-[#f8fafc] mb-4 text-[#cbd5e1]">Dewmini Karavita</h1>
     <hr/>
-    <p className="mt-10 text-xl text-white text-lg max-w-lg ">I'm Dewmini Karavita, an undergraduate IT student at the University of Sri Jayewardenepura, currently pursuing a BSc in ICT. With strong skills in UI/UX design, Python, C#, React, HTML, CSS, and SQL, I enjoy combining technical knowledge with creativity to build effective digital solutions. My key interests lie in UI/UX design and project management, areas where I can bring together both problem-solving and organizational skills. Looking ahead, my career goal is to grow into a role as a Project Manager, UI/UX Designer, or Business Analyst (BA), where I can contribute to impactful projects while continuously learning and evolving in the IT industry.</p>
+    <p className="mt-10 text-xl text-white text-lg max-w-lg ">I'm an undergraduate IT student at the University of Sri Jayewardenepura,
+          currently pursuing a BSc in ICT.Passionate about {" "} 
+          <span className="text-sky-400 font-medium"></span>and{" "}<span className="text-sky-400 font-medium">Project Management and UI/UX with skills in Python, C#, React, HTML, CSS, and SQL.Figma</span>, </p>
  </div>
 
 <div className='lg:w-1/3 ssm:w-fit'>
-  <img src={ProfilePicture} alt="Profile Picture" width={550} height={(550)} />
+  <img src={ProfilePicture} alt="Profile Picture" width={400} height={(400)} 
+  className="w-72 h-72 md:w-96 md:h-96 rounded-full shadow-lg object-cover border-4 border-sky-900 "/>
 </div>
 </section>
  );
