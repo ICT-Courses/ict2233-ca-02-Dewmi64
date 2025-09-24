2cab3137bf38ef589df1465b96c0c6acd9d96ea5
D.P. Karavita <ar112689@fhss.sjp.ac.lk>
2025-09-24 10:23:36 +0530

    Update project card component and add section header with title and description to Projects page

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -5,7 +5,30 @@ const ProjectCard = ({ project }) => {
     <div>
       <h3>{project.name}</h3>
       <p>{project.description || "No description available."}</p>
+      {project.topics && project.topics.length > 0 && (
+        <ul>
+          {project.topics.map((topic, index) => (
+            <li key={index}>{topic}</li>
+          ))}
+        </ul>
+      )}
+
+      {/* Links */}
+      <div>
+        {project.html_url && (
+          <a href={project.html_url} target="_blank" rel="noopener noreferrer">
+            GitHub
+          </a>
+        )}
+        {project.homepage && (
+          <a href={project.homepage} target="_blank" rel="noopener noreferrer" style={{ marginLeft: '10px' }}>
+            Live Demo
+          </a>
+        )}
+      </div>
+      
     </div>
+    
   );
 };
 
--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 const ProjectPage = () => {
   return (
     <div>
-      <h1>Project Page</h1>
+      <div className="text-center mb-12">
+          <h1 className="text-4xl md:text-5xl font-bold text-black mb-2">My Projects</h1>
+          <p className="text-lg md:text-xl">
+           Here are some of the projects I've built.</p>
+        </div>
     </div>
   );
 };
