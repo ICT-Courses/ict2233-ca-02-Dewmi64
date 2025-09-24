1e5e9bd9d15382302e08c21fc0820272ad7261a9
D.P. Karavita <ar112689@fhss.sjp.ac.lk>
2025-09-24 14:05:22 +0530

    Left alignment navigation links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 
+
 const Header=()=>{
-    return (<header className="fixed top-0 left-0 w-full z-50 flex justify-between items-center p-4 bg-black text-white " > <nav>
-        <ul className="flex space-x-4 ">
+    return (<header className="fixed top-0 left-0 w-full z-50 flex justify-between items-center p-4 bg-[#000000] text-white " > 
+    <div className="text-xl font-bold">My Portfolio</div>
+    <nav>
+        <ul className="flex space-x-4 ml-auto">
           <li><Link to="/">Home</Link></li>
           <li><Link to="/about">About</Link></li>
           <li><Link to="/projects">Project</Link></li>
--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -4,7 +4,7 @@ const ProjectPage = () => {
   return (
     <div>
       <div className="text-center mb-12">
-          <h1 className="text-4xl md:text-5xl font-bold text-black mb-2">My Projects</h1>
+          <h1 className="text-4xl md:text-5xl font-bold text-black mb-2 pt-20">My Projects</h1>
           <p className="text-lg md:text-xl">
            Here are some of the projects I've built.</p>
         </div>
